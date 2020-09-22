3befd1a5a017ccd6e23fa6f907f6d8beaa50c164
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-22 23:54:03 +0900

    Start: Practice useState

--- Class01/practice01/src/App.js
+++ Class01/practice01/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  var listData = ["apple", "banana", "car"];
+  var [listData2, setListDate2] = useState(["apple2", "banana2", "car2"]);
+  var [word, setWord] = useState("photo");
+  var [like, setLike] = useState([4, 7, 0]);
+
+  console.log(listData2);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>{word} TODO it!</h1>
       </header>
+      <ul>
+        <li>
+          {listData2[0]}
+          <button
+            onClick={() => {
+              setWord("Cat");
+            }}
+          >
+            click!
+          </button>
+          <button
+            onClick={() => {
+              setListDate2(["apple3", "banana2", "car2"]);
+            }}
+          >
+            3
+          </button>
+        </li>
+        <li>
+          {listData2[1]}
+          <button
+            onClick={() => {
+              var like2 = [...like];
+              like2[1]++;
+              setLike(like2);
+            }}
+          >
+            Like!
+          </button>
+          <span>{like[1]}</span>
+        </li>
+        <li>{listData2[2]}</li>
+      </ul>
     </div>
   );
 }
